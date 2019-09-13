ef690a2cbcf85eec1525ef1d003fbc6bf5e7823a
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-09-12 23:30:38 -0500

    DeleteCache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'aspnetcormaster-cache-v8';
+var CACHE_NAME = 'aspnetcormaster-cache-v1';
 var urlsToCache = [
    '/assets/css/style.css',
    '/assets/gitbook/style.css',
@@ -64,7 +64,7 @@ self.addEventListener('fetch', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-  var cacheWhitelist = ['dummy-cache-v8'];
+  var cacheWhitelist = ['dummy-cache-v1'];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {

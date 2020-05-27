ff4e5ee6b58236dbf5d6ee7cdca9b129e1ad646d
Benjamin Camacho <jahbenjah@hotmail.com>
2020-05-26 22:55:39 -0500

    se minifican assets

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+﻿﻿/// <binding Clean='clean' />
+"use strict";
+
+const gulp = require("gulp");
+const rimraf = require("rimraf");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const rename = require('gulp-rename');
+
+gulp.task('home', function () {
+  return gulp.src('assets/css/home.css')
+    .pipe(cleanCSS())
+    .pipe(rename('assets/css/home.min.css'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('plantillas', function () {
+  return gulp.src('assets/css/plantillas_estilo.css')
+    .pipe(cleanCSS())
+    .pipe(rename('assets/css/plantillas_estilo.min.css'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('main', function () {
+  return gulp.src('assets/js/main.js')
+    .pipe(uglify())
+    .pipe(rename('assets/js/main.min.js'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('main2', function () {
+  return gulp.src('assets/js/main2.js')
+    .pipe(uglify())
+    .pipe(rename('assets/js/main2.min.js'))
+    .pipe(gulp.dest('.'));
+});
+
+
+gulp.task("min", gulp.series(["home", "plantillas", "main", "main2"]));
